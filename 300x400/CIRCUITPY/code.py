5fe89be4fc8213b4de66d80dc1337284088a0bc3
edward sharp <edward@edwardsharp.net>
2025-12-28 14:22:31 -0500

    yank shallow display.py wrapper

--- 300x400/CIRCUITPY/code.py
+++ 300x400/CIRCUITPY/code.py
@@ -23,10 +23,11 @@ import socketpool
 import storage
 import wifi
 from digitalio import DigitalInOut
+from display.forecast_row import set_icon_loader
 
 # shared display functions
-from display.display import create_weather_layout, get_text_capacity
-from display.forecast_row import set_icon_loader
+from display.header import create_weather_layout
+from display.text_renderer import get_text_capacity
 from utils.logger import log
 from weather import weather_api
 from weather.weather_narrative import get_weather_narrative
